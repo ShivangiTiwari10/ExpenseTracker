7c9a1e8484cc13807623340d3411b699b2e4368e
Shivangi <shivangit1111@gmail.com>
2023-05-26 17:52:58 +0530

    feat: make splash screen

--- app/src/main/java/com/example/expencetracker/MainActivity.kt
+++ app/src/main/java/com/example/expencetracker/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.expencetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.expencetracker.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        activityScope.launch {
+
+            delay(3000)
+
+            val intent = Intent(this@MainActivity, ExpenseActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
         setContentView(binding.root)
     }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
 }
