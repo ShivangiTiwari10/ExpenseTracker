56c0bbda74eddb73d8f0a707b4cd1732728c0aff
Shivangi <shivangit1111@gmail.com>
2023-05-27 17:07:24 +0530

    feat: attached adapter to an activity

--- app/src/main/java/com/example/expencetracker/ExpenseActivity.kt
+++ app/src/main/java/com/example/expencetracker/ExpenseActivity.kt
@@ -1,11 +1,28 @@
 package com.example.expencetracker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.expencetracker.databinding.ActivityExpenseBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ExpenseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExpenseBinding
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expense)
+
+        binding = ActivityExpenseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = viewPagerAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf("Daily", "Monthly", "Yearly")
+
+            TabLayoutMediator(binding.tablayot, binding.viewPager) { tab, position ->
+                tab.text = tabTitles[position]
+            }.attach()
     }
 }
