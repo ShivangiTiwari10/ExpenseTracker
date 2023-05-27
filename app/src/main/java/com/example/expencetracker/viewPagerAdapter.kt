439f006341f8a64419463450077ca518f55322e2
Shivangi <shivangit1111@gmail.com>
2023-05-27 17:18:00 +0530

    refactor: adapter class

--- app/src/main/java/com/example/expencetracker/viewPagerAdapter.kt
+++ app/src/main/java/com/example/expencetracker/viewPagerAdapter.kt
@@ -6,18 +6,21 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.expencetracker.fragments.DailyFragment
 import com.example.expencetracker.fragments.MonthlyFragment
+import com.example.expencetracker.fragments.PenFragment
 import com.example.expencetracker.fragments.YearlyFragment
 
 class viewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):FragmentStateAdapter(fragmentManager,lifecycle) {
     override fun getItemCount(): Int {
-        return 3
+        return 4
     }
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> DailyFragment()
-            1 -> MonthlyFragment()
-            2 -> YearlyFragment()
+
+            0 -> PenFragment()
+            1 -> DailyFragment()
+            2 -> MonthlyFragment()
+            3 -> YearlyFragment()
             else -> throw IllegalArgumentException("Invalid tab position: $position")
         }
     }
