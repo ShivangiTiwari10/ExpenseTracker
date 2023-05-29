5572c6b62ac94d843bd1540579b2d42c99acd0e5
Shivangi <shivangit1111@gmail.com>
2023-05-29 16:14:08 +0530

    feat: set dialog on button click

--- app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
+++ app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
@@ -1,16 +1,21 @@
 package com.example.expencetracker.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.expencetracker.R
 import com.example.expencetracker.databinding.FragmentDailyBinding
 
 
 class DailyFragment : Fragment() {
 
     private lateinit var binding: FragmentDailyBinding
+
+    private lateinit var dialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +31,16 @@ class DailyFragment : Fragment() {
         // Inflate the layout for this fragment
 
         binding = FragmentDailyBinding.inflate(layoutInflater)
+
+        dialog = AlertDialog.Builder(requireContext()).setView(R.layout.edit_text_layout)
+            .setCancelable(false)
+            .create()
+        dialog.window?.setGravity(Gravity.BOTTOM)
+
+        binding.floatingActionButton.setOnClickListener {
+            dialog.show()
+        }
         return (binding.root)
-
-
 
     }
 
