6957f9d6676579ddfa2998108e0ac77a80e1b472
Shivangi <shivangit1111@gmail.com>
2023-05-31 17:32:39 +0530

    feat: get user input data

--- app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
+++ app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
@@ -1,25 +1,51 @@
 package com.example.expencetracker.fragments
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatEditText
-import com.example.expencetracker.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.expencetracker.Database.TrackerDatabase
+import com.example.expencetracker.EditNotes
+import com.example.expencetracker.model.Person
+import com.example.expencetracker.adapter.DailyfragmentAdapter
 import com.example.expencetracker.databinding.FragmentDailyBinding
+import com.example.expencetracker.model.trackerViewModel
 
 
-class DailyFragment : Fragment() {
+class DailyFragment : Fragment(), DailyfragmentAdapter.NotesClickedListener {
 
     private lateinit var binding: FragmentDailyBinding
 
-    private lateinit var dialog: AlertDialog
+    private lateinit var dataBase: TrackerDatabase
+    lateinit var viewModel: trackerViewModel
+    lateinit var adapter: DailyfragmentAdapter
+    lateinit var selectTracker: Person
+
+    private val updateTraker =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+                result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val expence = result.data?.getSerializableExtra("person") as? Person
+
+                if (expence != null) {
+                    viewModel.update(expence)
+                }
+
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         arguments?.let {
 
         }
@@ -33,22 +59,60 @@ class DailyFragment : Fragment() {
 
         binding = FragmentDailyBinding.inflate(layoutInflater)
 
-        dialog = AlertDialog.Builder(requireContext()).setView(R.layout.edit_text_layout)
-            .setCancelable(false)
-            .create()
-        dialog.window?.setGravity(Gravity.BOTTOM)
+        initUi()
 
-        val btnA = dialog.findViewById<AppCompatEditText>(R.id.textInputLayout)
-        val btnB = dialog.findViewById<AppCompatEditText>(R.id.amountInputLayout)
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[trackerViewModel::class.java]
 
-        binding.floatingActionButton.setOnClickListener {
-
-            dialog.show()
+        viewModel.allExpense.observe(viewLifecycleOwner) { list ->
+            list?.let {
+                adapter.updateList(list)
+            }
         }
-
-
+        dataBase = TrackerDatabase.getDataBase(requireContext())
         return (binding.root)
 
+    }
+
+    //
+    private fun initUi() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = DailyfragmentAdapter(requireContext(), this)
+
+
+        binding.recyclerView.adapter = adapter
+
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val note = result.data?.getSerializableExtra("person") as? Person
+
+                    if (note != null) {
+                        viewModel.insert(note)
+                    }
+                }
+            }
+        binding.floatingActionButton.setOnClickListener {
+            val intent = Intent(requireContext(), EditNotes::class.java)
+            getContent.launch(intent)
+            Log.d("Intent", "$intent")
+        }
+
+    }
+
+    override fun onItemClicked(note: Person) {
+
+        val intent = Intent(requireContext(), EditNotes::class.java)
+        intent.putExtra("expense_tracker", note)
+        updateTraker.launch(intent)
+    }
+
+
+    override fun onLongItemClicked(note: Person, cardView: CardView) {
+        TODO("Not yet implemented")
     }
 
 
