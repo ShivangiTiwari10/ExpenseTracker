0e6e912c4e8d237716527c964ba8878a2464928c
Shivangi <shivangit1111@gmail.com>
2023-06-01 11:35:37 +0530

    feat: set users edited data to get in DailyFragment recycler

--- app/src/main/java/com/example/expencetracker/EditNotes.kt
+++ app/src/main/java/com/example/expencetracker/EditNotes.kt
@@ -17,7 +17,7 @@ class EditNotes : AppCompatActivity() {
     private lateinit var person: Person
 
     private lateinit var old_note: Person
-    var isUpdate = false
+    private var isUpdate = false
 
     @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,57 +26,62 @@ class EditNotes : AppCompatActivity() {
         binding = ActivityEditNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        try {
-            old_note = intent.getSerializableExtra("expense_tracker") as Person
-            binding.textInputLayout.setText(old_note.text)
-            binding.amountInputLayout.setText(old_note.amount.toString())
-            binding.textDetail.setText(old_note.detail.toString())
-            isUpdate = true
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        binding.expencebtn.setOnClickListener {
 
-        binding.imageCheck.setOnClickListener {
-
-            val text = binding.textInputLayout.text.toString()
-            val amount = binding.amountInputLayout.text.toString()
-            val detail = binding.textDetail.text.toString()
-
-            if (text.isNotEmpty() || amount.isNotEmpty() || detail.isNotEmpty()) {
-
-                if (isUpdate) {
-                    person = Person(
-                        old_note.id, old_note.text, old_note.amount, old_note.detail
-                    )
-                } else {
-                    person = Person(
-                        null, text, amount, detail
-                    )
-                }
-                val intent = Intent()
-                intent.putExtra("person", person)
-                setResult(RESULT_OK, intent)
-            } else {
-                Toast.makeText(this@EditNotes, "Please enter some data", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-
+            try {
+                old_note = intent.getSerializableExtra("expense_tracker") as Person
+                binding.textInputLayout.setText(old_note.text)
+                binding.amountInputLayout.setText(old_note.amount.toString())
+                binding.textDetail.setText(old_note.detail.toString())
+                isUpdate = true
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
+            binding.imageCheck.setOnClickListener {
+
+                val text = binding.textInputLayout.text.toString()
+                val amount = binding.amountInputLayout.text.toString()
+                val detail = binding.textDetail.text.toString()
+
+                if (text.isNotEmpty() || amount.isNotEmpty() || detail.isNotEmpty()) {
+
+                    person = if (isUpdate) {
+                        Person(
+                            old_note.id, old_note.text, old_note.amount, old_note.detail
+                        )
+                    } else {
+                        Person(
+                            null, text, amount, detail
+                        )
+                    }
+                    val intent = Intent()
+                    intent.putExtra("person", person)
+                    setResult(RESULT_OK, intent)
+                } else {
+                    Toast.makeText(this@EditNotes, "Please enter some data", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+
+                }
+
+            }
+            val view: View? = this.currentFocus
+            if (view != null) {
+                // on below line we are creating a variable
+                // for input manager and initializing it.
+                val inputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+                // on below line hiding our keyboard.
+
+                inputMethodManager.hideSoftInputFromWindow(view.applicationWindowToken, 0)
+
+                // displaying toast message on below line.
+                Toast.makeText(this, "Key board hidden", Toast.LENGTH_SHORT).show()
+            }
         }
-        val view: View? = this.currentFocus
-        if (view != null) {
-            // on below line we are creating a variable
-            // for input manager and initializing it.
-            val inputMethodManager =
-                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
-            // on below line hiding our keyboard.
 
-            inputMethodManager.hideSoftInputFromWindow(view.applicationWindowToken, 0)
-
-            // displaying toast message on below line.
-            Toast.makeText(this, "Key board hidden", Toast.LENGTH_SHORT).show()
-        }
 
     }
 }
