8d0bc16b1ffc091aa1fc73ce1d1c6af4bcc15509
Shivangi <shivangit1111@gmail.com>
2023-06-06 19:10:47 +0530

    feat: set up observer to see income and expense

--- app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
+++ app/src/main/java/com/example/expencetracker/fragments/DailyFragment.kt
@@ -5,91 +5,101 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.expencetracker.Database.TrackerDatabase
 import com.example.expencetracker.EditNotes
-import com.example.expencetracker.model.Person
-import com.example.expencetracker.adapter.DailyfragmentAdapter
+import com.example.expencetracker.adapter.ExpenseAdapter
 import com.example.expencetracker.databinding.FragmentDailyBinding
+import com.example.expencetracker.model.Expense
+import com.example.expencetracker.model.Income
 import com.example.expencetracker.model.trackerViewModel
 
 
-class DailyFragment : Fragment(), DailyfragmentAdapter.NotesClickedListener {
+class DailyFragment : Fragment() {
 
     private lateinit var binding: FragmentDailyBinding
 
     private lateinit var dataBase: TrackerDatabase
     lateinit var viewModel: trackerViewModel
-    lateinit var adapter: DailyfragmentAdapter
-    lateinit var selectTracker: Person
 
-    private val updateTraker =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+    private lateinit var expenseAdapter: ArrayAdapter<Expense>
+    private lateinit var incomeAdapter: ArrayAdapter<Income>
 
-                result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val expence = result.data?.getSerializableExtra("person") as? Person
+    private var expenseTotal: Double = 0.0
 
-                if (expence != null) {
-                    viewModel.update(expence)
-                }
-
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         arguments?.let {
 
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-
         binding = FragmentDailyBinding.inflate(layoutInflater)
 
         initUi()
-
         viewModel = ViewModelProvider(
             this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
         )[trackerViewModel::class.java]
-
-        viewModel.allExpense.observe(viewLifecycleOwner) { list ->
-            list?.let {
-                adapter.updateList(list)
-            }
-        }
         dataBase = TrackerDatabase.getDataBase(requireContext())
-        return (binding.root)
+
+
+        setupExpenseListView()
+        setupIncomeListView()
+
+        return binding.root
+
 
     }
 
-    //
+    private fun setupExpenseListView() {
+        expenseAdapter = ExpenseAdapter(requireContext())
+        binding.expenseListView.adapter = expenseAdapter
+
+        viewModel.allExpense.observe(viewLifecycleOwner) { expenseList ->
+            expenseList?.let {
+                expenseAdapter.clear()
+                expenseAdapter.addAll(expenseList)
+                expenseTotal = expenseList.sumOf { it.amount!! }
+                binding.expenseAmount.text = expenseTotal.toString()
+            }
+        }
+    }
+
+    private fun setupIncomeListView() {
+        incomeAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1)
+        binding.incomeListView.adapter = incomeAdapter
+
+        viewModel.allInCome.observe(viewLifecycleOwner) { incomeList ->
+            incomeList?.let {
+                incomeAdapter.clear()
+                incomeAdapter.addAll(incomeList)
+            }
+        }
+    }
+
     private fun initUi() {
-        binding.recyclerView.setHasFixedSize(true)
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        adapter = DailyfragmentAdapter(requireContext(), this)
-
-
-        binding.recyclerView.adapter = adapter
 
         val getContent =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
-                    val note = result.data?.getSerializableExtra("person") as? Person
-
+                    val note = result.data?.getSerializableExtra("expense") as? Expense
+                    val note2 = result.data?.getSerializableExtra("income") as? Income
                     if (note != null) {
-                        viewModel.insert(note)
+                        viewModel.addExpense(note)
+                    }
+                    if (note2 != null) {
+                        viewModel.addIncome(note2)
                     }
                 }
             }
@@ -98,20 +108,12 @@ class DailyFragment : Fragment(), DailyfragmentAdapter.NotesClickedListener {
             getContent.launch(intent)
             Log.d("Intent", "$intent")
         }
-
     }
-
-    override fun onItemClicked(note: Person) {
-
-        val intent = Intent(requireContext(), EditNotes::class.java)
-        intent.putExtra("expense_tracker", note)
-        updateTraker.launch(intent)
-    }
-
-
-    override fun onLongItemClicked(note: Person, cardView: CardView) {
-        TODO("Not yet implemented")
-    }
-
 
 }
+
+
+
+
+
+
