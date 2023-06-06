b036a196489a070cdc58d93f6d26b33e0322d743
Shivangi <shivangit1111@gmail.com>
2023-06-06 16:01:41 +0530

    feat: create database for income and expense

--- app/src/main/java/com/example/expencetracker/Database/TrackerDatabase.kt
+++ app/src/main/java/com/example/expencetracker/Database/TrackerDatabase.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.expencetracker.model.Person
+import com.example.expencetracker.model.Expense
+import com.example.expencetracker.model.Income
 import com.example.expencetracker.utilities.DATABASE_NAME
 
 
-@Database(entities = arrayOf(Person::class), version = 1, exportSchema = false)
+@Database(entities = [Expense::class, Income::class], version = 2)
 abstract class TrackerDatabase : RoomDatabase() {
 
     abstract fun getexpenceDao(): ExpenseDao
@@ -18,15 +19,21 @@ abstract class TrackerDatabase : RoomDatabase() {
         private var INSTANCE: TrackerDatabase? = null
 
         fun getDataBase(context: Context): TrackerDatabase {
-            return INSTANCE ?: synchronized(this) {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
                 val instance = Room.databaseBuilder(
 
                     context.applicationContext, TrackerDatabase::class.java,
                     DATABASE_NAME
-                ).build()
+                ) .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
-                instance
+                return instance
             }
+
         }
     }
 }
