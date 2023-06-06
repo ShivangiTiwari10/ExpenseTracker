3b4e77a51a33582d3e660ab7f7508ac7984c8872
Shivangi <shivangit1111@gmail.com>
2023-06-06 16:02:33 +0530

    feat: create dataClass for income and expense

--- app/src/main/java/com/example/expencetracker/model/Expense.kt
+++ app/src/main/java/com/example/expencetracker/model/Expense.kt
@@ -5,11 +5,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "expense_tracker")
-data class Person(
+data class Expense(
 
     @PrimaryKey(autoGenerate = true) val id: Int?,
     @ColumnInfo(name ="text")val text:String?,
-    @ColumnInfo(name="amount") val amount:String?,
+    @ColumnInfo(name="amount") val amount:Double?,
     @ColumnInfo(name="detail") val detail:String?
 
 ): java.io.Serializable
---
+++ app/src/main/java/com/example/expencetracker/model/Income.kt
@@ -0,0 +1,17 @@
+package com.example.expencetracker.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "income_tracker")
+data class Income(
+
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name ="text")val text:String?,
+    @ColumnInfo(name="amount") val amount:Double?,
+    @ColumnInfo(name="detail") val detail:String?
+
+
+): java.io.Serializable
