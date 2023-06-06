f6513008ba8d4c123eddb2c4086843f8a97a83b7
Shivangi <shivangit1111@gmail.com>
2023-06-06 19:05:42 +0530

    feat: create custom adapter for expense list

---
+++ app/src/main/java/com/example/expencetracker/adapter/ExpenseAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.expencetracker.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.expencetracker.R
+import com.example.expencetracker.model.Expense
+
+class ExpenseAdapter(context: Context) : ArrayAdapter<Expense>(
+    context, R.layout.edit_text_layout
+) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var itemView = convertView
+        if (itemView == null) {
+            itemView = LayoutInflater.from(context).inflate(R.layout.edit_text_layout, parent, false)
+        }
+
+        val expense = getItem(position)
+        // Set the data to the views in your custom layout
+        // Example:
+        val expenseText = itemView?.findViewById<TextView>(R.id.text)
+        val expenseAmount = itemView?.findViewById<TextView>(R.id.tv_Amount)
+        if (expenseText != null) {
+            expenseText.text = expense?.text
+        }
+        if (expenseAmount != null) {
+            expenseAmount.text = expense?.amount.toString()
+        }
+
+        return itemView!!
+    }
+
+
+}
