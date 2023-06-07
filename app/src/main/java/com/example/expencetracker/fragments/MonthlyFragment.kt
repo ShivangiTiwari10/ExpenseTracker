771e15127c46ae04af6ec9c5d1a72b6926f5ecd8
Shivangi <shivangit1111@gmail.com>
2023-06-07 16:18:19 +0530

    feat: set date using date picker

--- app/src/main/java/com/example/expencetracker/fragments/MonthlyFragment.kt
+++ app/src/main/java/com/example/expencetracker/fragments/MonthlyFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TextView
 import com.example.expencetracker.databinding.FragmentMonthlyBinding
+import java.util.Calendar
 
 
 class MonthlyFragment : Fragment() {
 
     private lateinit var binding: FragmentMonthlyBinding
+
+    lateinit var dateTV: TextView
+    lateinit var datepicker: DatePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,48 @@ class MonthlyFragment : Fragment() {
         // Inflate the layout for this fragment
 
         binding = FragmentMonthlyBinding.inflate(layoutInflater)
+
+        dateTV = binding.idTVDate
+        datepicker = binding.datePicker
+
+        val today = Calendar.getInstance()
+
+        // Set the initial date text
+        val initialMonth = today.get(Calendar.MONTH) + 1
+        dateTV.text = initialMonth.toString()
+
+        // Set the TextView click listener
+        dateTV.setOnClickListener {
+            toggleDatePickerVisibility()
+        }
+
+        datepicker.init(
+            today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { _, year, month, day ->
+            val Date = (day.toString() + "-"
+                    + (month + 1) + "-" + year)
+
+            dateTV.text = Date
+
+            toggleDatePickerVisibility()
+        }
+
         return binding.root
+
     }
+
+    private fun toggleDatePickerVisibility() {
+
+        if (datepicker.visibility == View.VISIBLE) {
+            datepicker.visibility = View.GONE
+        } else {
+            datepicker.visibility = View.VISIBLE
+        }
+    }
+
 }
+
+
+
+
